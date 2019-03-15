9ad7193c9f39ca1558ddfc79b559a6ff3bde306c
Ali <mino.ict@gmail.com>
2019-03-15 18:49:13 +0200

    Fixing error when adding new post

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('posts.index');
 });
 Auth::routes();
 
@@ -21,14 +21,14 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/posts', 'PostsController@index')->name('posts.index');
     Route::get('/posts/restore', 'PostsController@restore')->name('posts.restore');
     Route::get('/posts/create', 'PostsController@create')->name('posts.create');
-    Route::post('/posts','PostsController@store')->name('posts.store');
+   Route::post('/posts','PostsController@store')->name('posts.store');
     Route::delete('/posts/{post}','PostsController@destroy')->name('posts.destroy');
     Route::get('/posts/{post}/edit','PostsController@edit')->name('posts.edit');
     Route::put('/posts/{post}','PostsController@update')->name('posts.update');
     Route::get('/posts/{post}','PostsController@show')->name('posts.show');
     
     Route::get('/posts/{post}/comment/create','CommentsController@create')->name('comments.create');
-    Route::post('/posts','CommentsController@store')->name('comments.store');
+    Route::post('/comments','CommentsController@store')->name('comments.store');
 
     Route::get('/home', 'HomeController@index')->name('home');
 });
