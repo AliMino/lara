ead603ac711ef63dd6f293dd5b9cc058e7e3e840
Ali <mino.ict@gmail.com>
2019-03-17 17:15:13 +0200

    Add PostResource and PostsController

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -14,13 +14,14 @@ class PostResource extends JsonResource
      */
     public function toArray($request)
     {
-        // return parent::toArray($request);
-        reurn [
+        return [
             'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
             'created_at' => $this->created_at->toDateString(),
             'user_id' => $this->user_id,
+            
+            // 'user' => new UserResource($this->user),
         ];
     }
 }
